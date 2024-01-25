f17ba6b3efa7a9868e3916719d978310ccfc582e
DH4NN <olusatimmie07@gmail.com>
2024-01-25 23:31:16 +0100

    Updating the frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,14 @@
+// const path = require('path');
+
+export default {
+  entry: './src/signin.html',  // Specify the entry point of your application
+  output: {
+    // filename: 'bundle.js',
+    // path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      // Add any necessary loaders for your project (e.g., for Babel, CSS, etc.)
+    ],
+  },
+};
